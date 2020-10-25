eb57906daa30f15df6ded45adb6c4f0254be28aa
german nunez <german90nunez@gmail.com>
2020-10-24 20:44:58 -0500

    register and login forms created. validations created and displaying errors, avatar upload fucntioning

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './upload'});
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,4 +15,39 @@ router.get('/login', function(req, res, next) {
   res.render('login', {title: 'Login'});
 });
 
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+  
+  if(req.file){
+    var profileimage = req.file.filename;
+  } else {
+    var profileimage = 'noimage.jpg'
+  }
+
+  // Form validator
+
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email is not valis').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+
+  // Check Errors
+  var errors = req.validationErrors();
+  if(errors){
+    res.render('register', {
+      errors: errors
+    })
+  } else {
+    console.log('No errrors')
+  }
+});
 module.exports = router;
